244207313ab1aa01afdd70264c035d9b6d338d91
Mistiry <mhlewisjr@gmail.com>
2018-02-24 02:58:24 -0600

    working on getting voice checking to run properly

--- bot.php
+++ bot.php
@@ -71,7 +71,8 @@ $shouldhavevoice = createShouldBeVoicedArray();
 
 while(1) {
     while($data = fgets($socket)) {
-		usleep(50000);
+		//usleep(50000);
+		sleep(2);
 		$timestamp = date("Y-m-d H:i:s T");
 		$ircdata = processIRCdata($data);
 		if(!in_array($ircdata['messagetype'], $ignore)) {
